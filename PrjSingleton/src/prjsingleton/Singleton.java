0aad8873ce794222cd249e57ddfd78715f2831f0
317122368 <317122368@BRPLI0108.animaedu.intranet>
2018-02-23 22:21:54 -0300

    start

---
+++ PrjSingleton/src/prjsingleton/Singleton.java
@@ -0,0 +1,39 @@
+/*
+ *Ref.
+*DEITEL, Harvey and Paul.
+*Java How to program. 4th Edition
+*2002. Prenteci Hall Inc. Page 496
+*/
+package prjsingleton;
+import javax.swing.*;
+/**
+ *
+ * @author 317122368
+ */
+public final class Singleton {
+    //(1)Por que a classe foi definida como Final?
+    private static Singleton singleton;
+    //(2) O que significa o objeto "singleton"?
+    
+    //(3) Por que criar um construtor privado?
+    private Singleton(){
+        JOptionPane.showMessageDialog(null, 
+                "Mensagem do construtor\n\nSingleton foi criado.","Sucesso!",2);
+    }
+    
+    //(4) Por que foi criado o método getSingletonInstance()?
+    public static Singleton getSingletonInstance(){
+        if (singleton == null)
+            singleton = new Singleton();
+        return singleton;
+        //(5) Explicar o if para criação do "singleton"
+    }
+    
+    /*
+    *Método apenas ilustrativo
+    *Não é necessário implementar o método...
+    */
+    public void abrirConexao(){
+        //...
+    }
+}
---
+++ PrjSingleton/src/prjsingleton/TesteSingleton.java
@@ -0,0 +1,36 @@
+/*
+ *Ref.
+*DEITEL, Harvey and Paul.
+*Java How to program. 4th Edition
+*2002. Prenteci Hall Inc. Page 496
+*/
+package prjsingleton;
+import javax.swing.*;
+/**
+ *
+ * @author 317122368
+ */
+public class TesteSingleton {
+    public static void main(String[] args){
+        //Declara referências
+        Singleton singleton1;
+        Singleton singleton2;
+        //Cria objetos
+        singleton1 = Singleton.getSingletonInstance();
+        singleton2 = Singleton.getSingletonInstance();
+        
+        //Criando terceiro objeto, alterar o construtor para public
+        //Singleton singleton3= new Singleton();
+        
+        System.out.println("Endereço singleton 1: "+singleton1.hashCode());
+        System.out.println("Endereço singleton 2: "+singleton2.hashCode());
+        //System.out.println("Endereço singleton 3: "+singleton3.hashCode());
+        
+        //Por que getSingletonInstance() foi chamado diretamente da classe?
+        if (singleton1 == singleton2)
+            JOptionPane.showMessageDialog(null, 
+                    "ATENÇÃO! "
+                            + "singleton1 e singleton2 "
+                            + "se referem a mesma instância de Singleton.");
+    }
+}
